68def6f46a6e3ca0523256bf1209c2c6f238618c
tomas fontanarrosa <tomasfontanarrosa@tomass-MacBook-Air.local>
2025-10-02 09:38:28 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/resto-compras-front-demo/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
